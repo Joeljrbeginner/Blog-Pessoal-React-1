4b92f3c442eab304ff1e94c49eebbc49f1d67e80
Joeljrbeginner <joeljuniormooca364@gmail.com>
2023-02-24 17:09:29 -0300

    criando projeto react

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Olá Mundo ! Viado kkkk
+         Olá Mundo è nois na fita!
         </p>
         <a
           className="App-link"
